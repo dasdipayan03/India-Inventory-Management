bb8705ae5758c9b1caa072e3c9bcad889af8342c
dasdipayan03 <dasdipayan03@gmail.com>
2025-09-27 23:50:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,12 +3,16 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Required for Render
+    rejectUnauthorized: false, // Required for Render free DB
   },
 });
 
 pool.connect()
   .then(() => console.log("✅ Connected to PostgreSQL"))
-  .catch(err => console.error("❌ DB Connection Error:", err));
+  .catch(err => {
+    console.error("❌ DB Connection Error:");
+    console.error("Message:", err.message);
+    console.error("Stack:", err.stack);
+  });
 
 module.exports = pool;
