7bbe24fe8623c67671038d0d1354c4369d10d9d0
dasdipayan03 <dasdipayan03@gmail.com>
2025-09-28 22:54:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
 
-console.log(">>> From db.js, DB_URL =", process.env.DB_URL);
+console.log(">>> From db.js, DATABASE_URL =", process.env.DATABASE_URL);
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false, // required for Render
