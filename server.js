6c856658bf3fb1dc5bd91a33a80baec857dea8f0
dasdipayan03 <dasdipayan03@gmail.com>
2025-10-05 12:12:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use((req, res) => {
 });
 
 // -------------------- START SERVER --------------------
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
